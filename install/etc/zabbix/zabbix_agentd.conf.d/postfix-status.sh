18f98d0fa9eb347afca872d0a130114964ce19a8
Dave Conroy <dave@tiredofit.ca>
2020-06-27 10:57:51 -0700

    Fix for groups expansion

--- install/etc/zabbix/zabbix_agentd.conf.d/postfix-status.sh
+++ install/etc/zabbix/zabbix_agentd.conf.d/postfix-status.sh
@@ -8,11 +8,11 @@ ZABBIX_CONF=/etc/zabbix/zabbix_agentd.conf
 DEBUG=0
 
 function zsend {
-  key="postfix[`echo "$1" | tr ' -' '_' | tr '[A-Z]' '[a-z]' | tr -cd [a-z_]`]"
-  value=`grep -m 1 "$1" $DAT2 | awk '{print $1}'`
+  key="postfix[$(echo "$1" | tr ' -' '_' | tr '[A-Z]' '[a-z]' | tr -cd [a-z_])]"
+  value=$(grep -m 1 "$1" "$DAT2" | awk '{print $1}')
   if [ ${DEBUG} -ne 0 ] ; then
-    echo "Send key "${key}" with value "${value}"" >&2
-    /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}" 
+    echo "Send key '${key}' with value '${value}'" >&2
+    /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}"
   else
     /usr/bin/zabbix_sender -c $ZABBIX_CONF -k "${key}" -o "${value}" 2>&1 >/dev/null
   fi
@@ -20,14 +20,14 @@ function zsend {
 
 # http://jimsun.linxnet.com/downloads/ChangeLog-1.1.4
 PFLOGSUMM_VERSION="$($PFLOGSUMM --version | sed 's/[a-zA-Z\.\ ]//g')"
-if [ $PFLOGSUMM_VERSION -eq 113 ] ; then
+if [ "$PFLOGSUMM_VERSION" -eq 113 ] ; then
   PFLOGSUMM_PARAMS='--smtpd-stats --bounce_detail=0 --deferral_detail=0 --reject_detail=0 --no_no_msg_size --smtpd_warning_detail=0'
-elif [ $PFLOGSUMM_VERSION -gt 113 ] ; then
+elif [ "$PFLOGSUMM_VERSION" -gt 113 ] ; then
   PFLOGSUMM_PARAMS='--smtpd-stats --bounce-detail=0 --deferral-detail=0 --reject-detail=0 --no-no-msg-size --smtpd-warning-detail=0'
 else
   PFLOGSUMM_PARAMS='--smtpd_stats --no_bounce_detail --no_deferral_detail --no_reject_detail --no_no_msg_size --no_smtpd_warnings'
 fi
-/usr/sbin/logtail -f$MAILLOG -o$DAT1 | $PFLOGSUMM -h 0 -u 0 $PFLOGSUMM_PARAMS > $DAT2
+/usr/sbin/logtail -f$MAILLOG -o$DAT1 | $PFLOGSUMM -h 0 -u 0 "$PFLOGSUMM_PARAMS" > "$DAT2"
 
 zsend received
 zsend delivered
@@ -44,5 +44,5 @@ zsend recipients
 zsend "bytes received"
 zsend "bytes delivered"
 
-rm -f $DAT2
+rm -f "$DAT2"
 
